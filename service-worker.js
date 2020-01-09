d937ee6eac192a3bb5fffc51d466d9388ff949d5
naga <nagasundaram4@users.noreply.github.com>
2020-01-10 01:53:25 +0800

    changes

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-importScripts("/amd/precache-manifest.d1ec16ba517281ebb506784ba0f8927a.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("/amd/precache-manifest.790ae33b298c11ab58fee291c2cab5e8.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 // This is the code piece that GenerateSW mode can't provide for us.
 // This code listens for the user's confirmation to update the app.
